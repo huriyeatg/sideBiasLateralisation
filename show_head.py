dd36e16ad6863a716cfc5d72fd1f8167f254bdb6
Candela Medina <candela.medina@dpag.ox.ac.uk>
2025-05-09 13:17:37 +0100

    init testing

--- show_head.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy as np
-import main_funcs as mfun
-
-# Crear una instancia de la clase analysis
-info = mfun.analysis()
-
-# Mostrar las primeras filas
-print(info.recordingList.head()) 
